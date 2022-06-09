389859fc07463a7f45a8109061fea61e3294c599
ekye121 <ekye0121@gmail.com>
2022-06-09 14:52:19 -0400

    added hover to footer buttons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 function Footer() {
 	return (
@@ -9,15 +10,24 @@ function Footer() {
 				width: "100%",
 			}}
 		>
-			<div className="d-flex justify-content-around align-items-center text-white w-100 h-100">
+			<div
+				className="d-flex justify-content-around align-items-center text-white w-100 h-100"
+				style={{ color: "#6c757d" }}
+			>
 				<div>
-					<div>SUPPORT</div>
+					<OverlayTrigger overlay={<Tooltip>Coming soon</Tooltip>}>
+						<h5 style={{ cursor: "pointer" }}>SUPPORT</h5>
+					</OverlayTrigger>
 				</div>
 				<div>
-					<div>ABOUT</div>
+					<OverlayTrigger overlay={<Tooltip>Coming soon</Tooltip>}>
+						<h5 style={{ cursor: "pointer" }}>ABOUT</h5>
+					</OverlayTrigger>
 				</div>
 				<div>
-					<div>SOCIAL MEDIA</div>
+					<OverlayTrigger overlay={<Tooltip>Coming soon</Tooltip>}>
+						<h5 style={{ cursor: "pointer" }}>SOCIAL MEDIA</h5>
+					</OverlayTrigger>
 				</div>
 			</div>
 		</footer>
